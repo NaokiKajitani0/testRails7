b414f5f9709562ee62a4dfc229537bedde68692d
NaokiKajitani0 <n.k.engineer0@gmail.com>
2022-08-13 11:35:34 +0900

    tailwindをインストールするもうまくいかず

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_071555) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_014309) do
+  create_table "todos", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.date "limit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "contents"
